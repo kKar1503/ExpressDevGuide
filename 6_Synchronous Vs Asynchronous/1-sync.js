20766f75ef313a58a6cec5dddfd81dc61030efb3
Kar <karlok42.21@ichat.sp.edu.sg>
2022-06-26 14:10:58 +0800

    more uploads

---
+++ 6_Synchronous Vs Asynchronous/1-sync.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const data = fs.readFileSync('TextFile.txt', 'utf8');
+
+console.log('File data is', data);
+
+console.log('TEST');
---
+++ 6_Synchronous Vs Asynchronous/2-callback.js
@@ -0,0 +1,5 @@
+const HelloUser = (name) => {
+	console.log("Hello " + name);
+};
+
+module.exports = { HelloUser };
